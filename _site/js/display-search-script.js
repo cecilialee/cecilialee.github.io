a357818c9a30eb98aacea40835a39b240c0e5c34
Cecilia Lee <ceciliahslee@gmail.com>
2017-12-03 12:48:40 +0800

    Apply instant search function to all pages

--- _site/js/display-search-script.js
+++ _site/js/display-search-script.js
@@ -1,15 +1,15 @@
 // Dynamic display of search results
 
 const searchInput = document.getElementById('search-input');
-const home = document.getElementById('home');
+const content = document.getElementById('content');
 const results = document.getElementById('results');
 
 searchInput.addEventListener('input', function() {
     if (this.value) {
-        home.style.display = 'none';
+        content.style.display = 'none';
         results.style.display = 'inline';
-    } else if (!this.value && home.style.display === 'none') {
-      home.style.display = 'block';
+    } else if (!this.value && content.style.display === 'none') {
+      content.style.display = 'block';
       results.style.display = 'none';
     }
 });
